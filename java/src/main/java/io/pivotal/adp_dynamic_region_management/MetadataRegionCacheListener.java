fa1decec5e85ff8634adffc7d5ae6d2601103f4c
Randy May <rmay@pivotal.io>
2015-02-05 15:49:00 -0500

    add option to override redundancy-recovery-delay and startup-redundancy-recovery-delay in MetadataRegionCacheListener

--- java/src/main/java/io/pivotal/adp_dynamic_region_management/MetadataRegionCacheListener.java
+++ java/src/main/java/io/pivotal/adp_dynamic_region_management/MetadataRegionCacheListener.java
@@ -2,7 +2,6 @@ package io.pivotal.adp_dynamic_region_management;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Properties;
 
 import com.gemstone.gemfire.cache.Cache;
@@ -15,11 +14,21 @@ import com.gemstone.gemfire.cache.RegionExistsException;
 import com.gemstone.gemfire.cache.RegionFactory;
 import com.gemstone.gemfire.cache.util.CacheListenerAdapter;
 import com.gemstone.gemfire.pdx.PdxInstance;
+import com.gemstone.gemfire.pdx.WritablePdxInstance;
 
 public class MetadataRegionCacheListener extends CacheListenerAdapter<String,PdxInstance>  implements Declarable {
 
+	private static String FAILURE_REDUNDANCY_RECOVERY_DELAY_PARAMETER = "FAILURE_REDUNDANCY_RECOVERY_DELAY";
+	private static String STARTUP_REDUNDANCY_RECOVERY_DELAY_PARAMETER = "STARTUP_REDUNDANCY_RECOVERY_DELAY";
+	
+	private static String PARTITION_ATTRIBUTES_FIELD = "partitionAttributes";
+	private static String RECOVERY_DELAY_FIELD = "recoveryDelay";
+	private static String STARTUP_RECOVERY_DELAY_FIELD = "startupRecoveryDelay";
+	
     private Cache cache;
     private DistributionPolicy distributionPolicy = null;
+    private int redundancyRecoveryDelay = Integer.MIN_VALUE; //indicates not set
+    private int startupRedundancyRecoveryDelay = Integer.MIN_VALUE; //indicates not set
 
     public MetadataRegionCacheListener() {
         this.cache = CacheFactory.getAnyInstance();
@@ -47,9 +56,31 @@ public class MetadataRegionCacheListener extends CacheListenerAdapter<String,Pdx
 
     private void createRegion(String regionName, PdxInstance pdxInstance) {
         PdxInstance serverOptions = (PdxInstance) pdxInstance.getField("server");
+
+        // enforce overrides by setting server options here
+        if (redundancyRecoveryDelay != Integer.MIN_VALUE){
+        	PdxInstance partitionAttributes  = (PdxInstance) serverOptions.getField(PARTITION_ATTRIBUTES_FIELD); 
+        	WritablePdxInstance writeable =  serverOptions.createWriter();
+        	writeable.setField(RECOVERY_DELAY_FIELD, Integer.valueOf(redundancyRecoveryDelay));
+        	serverOptions = writeable;
+        }
+        
+        if (startupRedundancyRecoveryDelay != Integer.MIN_VALUE ){
+        	WritablePdxInstance writeable = null;
+        	if ( serverOptions instanceof WritablePdxInstance){
+        		writeable = (WritablePdxInstance) serverOptions;
+        	} else {
+        		writeable = serverOptions.createWriter();
+        	}
+        	
+        	writeable.setField(STARTUP_RECOVERY_DELAY_FIELD, Integer.valueOf(startupRedundancyRecoveryDelay));
+        	serverOptions = writeable;
+        }
+        
         RegionOptionsFactory regionOptionsFactory = new RegionOptionsFactory(serverOptions, distributionPolicy);
         RegionFactory regionFactory = regionOptionsFactory.getRegionFactory();
 
+        
         logInfo(">> MetadataRegionCacheListener creating region named: " + regionName);
 
         try {
@@ -82,6 +113,25 @@ public class MetadataRegionCacheListener extends CacheListenerAdapter<String,Pdx
     		} catch(InstantiationException | IllegalAccessException xx){
     			throw new RuntimeException("distributionPolicy class was found but instantiation failed: " + className, xx);
     		} 
+    	}
+    	
+    	// capture properties
+    	String s = properties.getProperty(FAILURE_REDUNDANCY_RECOVERY_DELAY_PARAMETER);
+    	if (s != null){
+    		try {
+    			redundancyRecoveryDelay = Integer.parseInt(s);
+    		} catch(NumberFormatException x){
+    			throw new RuntimeException(FAILURE_REDUNDANCY_RECOVERY_DELAY_PARAMETER + " property must be an integer: " + s);
+    		}
+    	}
+    	
+    	s = properties.getProperty(STARTUP_REDUNDANCY_RECOVERY_DELAY_PARAMETER);
+    	if (s != null){
+    		try {
+    			startupRedundancyRecoveryDelay = Integer.parseInt(s);
+    		} catch(NumberFormatException x){
+    			throw new RuntimeException(STARTUP_REDUNDANCY_RECOVERY_DELAY_PARAMETER + " property must be an integer: " + s);
+    		}
     	}
     }
 }
