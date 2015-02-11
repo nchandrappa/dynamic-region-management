59fc2ce77a2fc1cec8920834f2db5a34662a81cf
Randy May <rmay@pivotal.io>
2015-02-11 13:49:34 -0500

    increase integration test timeouts

--- spec/integration/destroyRegionSpec.js
+++ spec/integration/destroyRegionSpec.js
@@ -15,7 +15,7 @@ feature("Dynamic region destruction", function() {
 
   beforeEach(function() {
     originalDefaultTimeoutInterval = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
   });
 
   afterEach(function() {
