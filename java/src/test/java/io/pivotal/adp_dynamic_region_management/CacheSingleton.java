d4a86a972789fbfea7e284320193526680aab8a2
Neil Stevenson <neil.stevenson-contractor@adp.com>
2015-04-13 10:54:03 +0100

    Derive path so works on Vagrant or not

--- java/src/test/java/io/pivotal/adp_dynamic_region_management/CacheSingleton.java
+++ java/src/test/java/io/pivotal/adp_dynamic_region_management/CacheSingleton.java
@@ -2,12 +2,16 @@ package io.pivotal.adp_dynamic_region_management;
 
 import com.gemstone.gemfire.cache.Cache;
 import com.gemstone.gemfire.cache.CacheFactory;
+import java.nio.file.*;
 
 public abstract class CacheSingleton {
     static private Cache cache;
 
     public static Cache getCache() {
+    	
         if(cache == null) {
+        	CacheSingleton.setSystemPropertiesForJunit();
+        	
             CacheFactory cacheFactory = new CacheFactory();
             cacheFactory.set("cache-xml-file", "src/test/resources/test.xml");
             cacheFactory.setPdxPersistent(true);
@@ -16,4 +20,16 @@ public abstract class CacheSingleton {
 
         return cache;
     }
+    
+    /* So that can run Junit from Maven build inside or outside of Vagrant VM.
+     */
+    public static void setSystemPropertiesForJunit() {    	
+    	Path directory = Paths.get("/vagrant/tmp");
+    	if(Files.isDirectory(directory)) {
+           	System.setProperty("JAVA_TEST_DISK_STORE", "/vagrant/tmp/javaTestDiskStore");
+    	} else {
+           	System.setProperty("JAVA_TEST_DISK_STORE", "target" + System.getProperty("file.separator") + "javaTestDiskStore");
+    	}    	
+    }
+    
 }
