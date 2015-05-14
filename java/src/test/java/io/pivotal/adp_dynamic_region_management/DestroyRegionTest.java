5deabbb4419811b18097f85c33a0b9af902b36c7
Neil Stevenson <neil.stevenson-contractor@adp.com>
2015-05-14 13:09:11 +0100

    Master version supercedes

--- java/src/test/java/io/pivotal/adp_dynamic_region_management/DestroyRegionTest.java
+++ java/src/test/java/io/pivotal/adp_dynamic_region_management/DestroyRegionTest.java
@@ -6,16 +6,20 @@ import com.gemstone.gemfire.cache.execute.FunctionContext;
 import com.gemstone.gemfire.cache.execute.ResultSender;
 import com.gemstone.gemfire.pdx.JSONFormatter;
 import com.gemstone.gemfire.pdx.PdxInstance;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -28,6 +32,8 @@ import static org.mockito.Mockito.when;
 public class DestroyRegionTest {
     @Rule
     public TestName name = new TestName();
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
     public String regionName;
 
@@ -37,11 +43,11 @@ public class DestroyRegionTest {
     FunctionContext context;
 
     @Mock
-    ResultSender resultSender;
+    ResultSender<Object> resultSender;
 
     @Before
     public void setUp() {
-        regionName = getClass().getName() + name.getMethodName();
+        regionName = getClass().getSimpleName() + name.getMethodName();
         cache = CacheSingleton.getCache();
         GemfireFunctionHelper.rethrowFunctionExceptions(resultSender);
     }
@@ -55,7 +61,7 @@ public class DestroyRegionTest {
     public void hasResult() throws Exception {
         assertThat(new DestroyRegion().hasResult(), equalTo(true));
     }
-
+    
     @Test
     public void executeSendsTrueResultOnSuccess() throws Exception {
         createRegion(regionName);
@@ -98,11 +104,12 @@ public class DestroyRegionTest {
         assertThat(cache.getRegion(regionName), equalTo(null));
     }
 
+    @SuppressWarnings("unchecked")
     @Test
     public void executeSendsExceptions() throws Exception {
         RuntimeException exception = new RuntimeException("This is my message");
 
-        ResultSender resultSender = mock(ResultSender.class);
+		ResultSender<Object> resultSender = mock(ResultSender.class);
 
         createRegion(regionName);
         when(context.getResultSender()).thenReturn(resultSender);
@@ -129,6 +136,121 @@ public class DestroyRegionTest {
         assertThat(metadataRegion.containsKey(regionName), equalTo(false));
     }
 
+    @Test
+    public void executeDestroysMetadataEvenIfRegionMissing() throws Exception {
+        createRegion(regionName);
+        cache.getRegion(regionName).destroyRegion();
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList(regionName));
+        new DestroyRegion().execute(context);
+
+        Region<String, PdxInstance> metadataRegion = MetadataRegion.getMetadataRegion();
+        assertThat(metadataRegion.containsKey(regionName), equalTo(false));
+    }
+
+    /* If no metadata entry, region wasn't created by Dynamic Region Management
+     * and so not valid to delete via Dynamic Region Management.
+     */
+    @Test
+    public void executeDestroysLeavesRegionIfMetadataMissing() throws Exception {
+        createRegion(regionName);
+        Region<String, PdxInstance> metadataRegion = MetadataRegion.getMetadataRegion();
+        metadataRegion.destroy(regionName);
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList(regionName));
+        new DestroyRegion().execute(context);
+
+        verify(resultSender).lastResult(false);
+    }
+
+    @Test
+    public void executeDestroyWithNullArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("One argument required in list");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(null);
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyWithWrongTypeArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("One argument required in list");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(new String("hello"));
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyWithWrongEmbeddedTypeArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("Region name must be non-empty String");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList(Integer.MAX_VALUE));
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyWithNullEmbeddedTypeArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("Region name must be non-empty String");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList((Object)null));
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyWithInvalidEmbeddedTypeArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("Region name must be non-empty String");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList(""));
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyWithEmptyListArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("One argument required in list");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(new ArrayList<String>());
+        new DestroyRegion().execute(context);
+    }
+
+    @Test
+    public void executeDestroyWithOverfullListArgs() {
+		expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("One argument required in list");
+
+    	List<String> args = new ArrayList<>();
+    	args.add("one");
+    	args.add("two");
+    	
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(args);
+        new DestroyRegion().execute(context);
+    }
+    
+    @Test
+    public void executeDestroyFailsForSubRegions() {
+    	String targetRegionName = regionName + Region.SEPARATOR_CHAR + regionName;
+
+    	expectedException.expect(RuntimeException.class);
+	    expectedException.expectMessage("Region name '" + targetRegionName + "' cannot include reserved char '/'");
+
+        when(context.getResultSender()).thenReturn(resultSender);
+        when(context.getArguments()).thenReturn(Arrays.asList(targetRegionName));
+        new DestroyRegion().execute(context);
+    }
+    
     private void createRegion(String name) {
         Region<String, PdxInstance> metadataRegion = MetadataRegion.getMetadataRegion();
         PdxInstance regionOptions = JSONFormatter.fromJSON("{ \"client\": { \"type\": \"CACHING_PROXY\" } }");
